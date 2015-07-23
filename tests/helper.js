33af8d5effab0275d5e3e5d5e1df18a0a7b1f409
James Messinger <james.messinger@gmail.com>
2015-07-23 13:16:55 -0500

    Updated test timeouts for Travis CI

--- tests/helper.js
+++ tests/helper.js
@@ -29,7 +29,7 @@ beforeEach(function(done) {
 
   // Increase the test timeout, since some tests have to allow time for multiple Browserify builds
   this.currentTest.timeout(5000);
-  this.currentTest.slow(2000);
+  this.currentTest.slow(3000);
 });
 
 /**
